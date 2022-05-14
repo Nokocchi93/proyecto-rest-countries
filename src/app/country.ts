f31ce186f9986e05422450f94a8f1a5f5cd85c38
Aitor Mateu <articuno1993@gmail.com>
2022-05-14 12:10:46 +0200

    Implement images for homepage

--- src/app/country.ts
+++ src/app/country.ts
@@ -1,11 +1,17 @@
 export interface Country {
-  name: {
+  name?: {
     common?: string;
     official?: string;
     nativeName?: any;
   };
-  currencies: any;
-  capital: string[];
-  borders: string[];
-  population: number;
+  currencies?: any;
+  capital?: string[];
+  borders?: string[];
+  population?: number;
+  flags?: {
+    png?: string;
+    svg?: string;
+  };
+  region?: string;
+  subregion?: string;
 }
